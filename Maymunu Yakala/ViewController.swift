de0086586e4f9b14218506bdb77bf1cf3299d45a
Macbook <macbook@Macbook-MacBook-Pro.local>
2017-08-26 22:02:59 +0300

    Initial Commit

---
+++ Maymunu Yakala/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maymunu Yakala
+//
+//  Created by Macbook on 26.08.2017.
+//  Copyright © 2017 LastShotGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
